d32f242cba63c1f9691f3b1fc960b3556f64c176
Szymon Drozdowski <szymo.dr@gmail.com>
2023-01-27 15:38:11 +0100

    done the rest stages of scrpit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function titleClickHandler(event){
-  const clickedElement = this;
+    event.preventDefault();
+    const clickedElement = this;
   console.log('Link was clicked!');
 
   /* [DONE] remove class 'active' from all article links  */
@@ -12,22 +13,32 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   clickedElement.classList.add('active')
   console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector)
+
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle)
 
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active')
+
 }
 
 const links = document.querySelectorAll('.titles a');
